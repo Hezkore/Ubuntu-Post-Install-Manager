7384c588d37a885461b85ff923e54d92a75d0a38
hezkore <hezkore@gmail.com>
2021-12-01 18:33:09 +0100

    general work

--- src/misc_menu.sh
+++ src/misc_menu.sh
@@ -5,6 +5,7 @@ function show_misc_menu () {
 	items=(
 		"Folder_Setup" "Create common folder directories" "ON"
 		"Nautilus_New_Docs" "Add 'New Document' menu to Nautilus" "ON"
+		"Nautilus_Admin" "Add 'Open as Admin' option to Nautilus" "OFF"
 	)
 	generate_selection_menu "Miscellaneous Options" "${items[@]}"
 }
@@ -33,4 +34,8 @@ function nautilus_new_docs () {
 	echo -e "SuperStrict\n\nFramework brl.standardio\n\nPrint(\"Hello World\")" > ~/Templates/Code/blitzmax.bmx
 	echo -e "#include <iostream>\n\nint main() {\n\tstd::cout << \"Hello World\";\n\treturn 0;\n}" > ~/Templates/Code/cpp.cpp
 	echo -e "#"'!'"/bin/bash\n# -*- Mode: sh; coding: utf-8; indent-tabs-mode: t; tab-width: 4 -*-\n\n# My bash script\n\necho \"Hello World\"" > ~/Templates/Code/bash.sh
+}
+
+function nautilus_admin () {
+	sudo apt install nautilus-admin -y
 }
