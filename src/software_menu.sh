3a6895ee5b18db03a19423ac321d40af1941b4bd
hezkore <hezkore@gmail.com>
2021-12-02 23:11:07 +0100

    updated edge

--- src/software_menu.sh
+++ src/software_menu.sh
@@ -481,14 +481,10 @@ function install_obs_nvfbc_plugin () {
 
 function install_edge_browser () {
 	if bin_exists "curl"; then
-	
-		## Setup
 		curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
 		sudo install -o root -g root -m 644 microsoft.gpg /etc/apt/trusted.gpg.d/
-		sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/edge stable main" > /etc/apt/sources.list.d/microsoft-edge-stable.list'
-		sudo rm microsoft.gpg
-		
-		## Install
+		sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/edge stable main" > /etc/apt/sources.list.d/edge.list'
+		sudo rm -f microsoft.gpg
 		sudo apt update
 		sudo apt install microsoft-edge-stable -y
 		
