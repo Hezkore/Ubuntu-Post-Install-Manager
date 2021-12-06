966aa69bb77e880e141fb3748bde1a457bfab890
hezkore <hezkore@gmail.com>
2021-12-06 11:47:59 +0100

    overhauled quake 3 install

--- src/gaming_menu.sh
+++ src/gaming_menu.sh
@@ -182,30 +182,96 @@ function install_protontricks () {
 }
 
 function install_quake_3 () {
-	if bin_exists "game-data-packager"; then
-		
-		if bin_exists "gdebi"; then
-			
-			echo "Downloading Quake 3 demo game data..."
-			
-			# There's no way to say yes to all, so pip in y
-			game-data-packager --gain-root-command sudo --install-method gdebi --no-search --verbose --no-compress -i quake3 <<< y
-			# Since there's no way to actually know WHAT was download, we have to wildcard delete the deb :S
-			rm -f quake3-*.deb
-			
-			echo
-			echo "Installing ioQuake 3..."
-			sudo apt install ioquake3 -y
-			
-			
-			return 0
+	if bin_exists "wget"; then
+		if bin_exists "unzip"; then
+			if bin_exists "game-data-packager"; then
+				if bin_exists "gdebi"; then
+					
+					q3dir="$HOME/.q3a"
+					
+					# Make sure the Q3 dir exists
+					echo "Quake 3 directory $q3dir"
+					mkdir -p "$q3dir"
+					
+					echo
+					echo "Downloading Quake 3 demo game data..."
+					# There's no way to say yes to all, so pipe in y
+					game-data-packager --gain-root-command sudo --install-method gdebi --no-search --verbose --no-compress -i quake3 <<< y
+					
+					echo
+					echo "Extracting Quake 3 demo game data..."
+					# Since there's no way to actually know WHAT was download, we have to wildcard delete the deb :S
+					rm -f quake3-*.deb
+					
+					echo
+					echo "Relocating Quake 3 demo game data..."
+					# Move the data from /usr/share/games/quake3-demo-data/demoq3 to our Q3 dir
+					sudo mv -f "/usr/share/games/quake3-demo-data/demoq3/" "$q3dir/baseq3/"
+					
+					# Take ownership of the game data
+					sudo chown -R $USER "$q3dir/baseq3"
+					
+					echo
+					echo "Downloading latest patch data..."
+					# Use the patch data as our base
+					wget -O patch.zip https://files.ioquake3.org/quake3-latest-pk3s.zip
+					
+					echo
+					echo "Extracting patch data..."
+					unzip -o -d "$q3dir" patch.zip
+					# Rename the patch dir to Quake 3
+					sudo cp -rf "$q3dir/quake3-latest-pk3s/baseq3" "$q3dir"
+					sudo cp -rf "$q3dir/quake3-latest-pk3s/missionpack" "$q3dir"
+					sudo rm -rf "$q3dir/quake3-latest-pk3s"
+					rm -f patch.zip
+					
+					# Take ownership of the patch data
+					sudo chown -R $USER "$q3dir"
+					
+					echo
+					echo "Downloading ioQuake 3..."
+					#sudo apt install ioquake3 -y # We want a newer version than this!
+					wget -O q3-linux.zip https://files.ioquake3.org/Linux.zip
+					
+					echo
+					echo "Extracting ioQuake 3..."
+					# First zip
+					unzip -o q3-linux.zip
+					sudo rm -rf q3-linux.zip
+					# Nested zip
+					unzip -o -d "$q3dir" release-linux*.zip
+					sudo rm -rf release-linux*.zip
+					
+					# Create a bogus CD Key for the demo
+					sudo echo -e "aaaaaaaaaaaaaaaa" > "$q3dir/baseq3/q3key"
+					
+					# Create a cute little bin script so the user can call 'quake3'
+					binscript="#!/bin/bash
+cd \"$HOME/.q3a\" && ./ioquake3.x86_64"
+					sudo echo -e "$binscript" > "$HOME/.local/bin/quake3 $@"
+					sudo chmod u+x "$HOME/.local/bin/quake3"
+					
+					# Check if a "Games" folder exists and add a Quake 3 shortcut
+					if [[ -d "$HOME/Games"  ]]; then
+						ln -s "$q3dir" "$HOME/Games/Quake 3"
+					fi
+					
+					return 0
+				else
+					LAST_ERROR="GDebi is not installed, cannot extract DEB package"
+					return 1
+				fi
+				return 0
+			else
+				LAST_ERROR="Game Data Packer is not installed, cannot download game data"
+				return 1
+	fi
 		else
-			LAST_ERROR="GDebi is not installed, cannot extract DEB package"
+			LAST_ERROR="UnZip is not installed, cannot extract game data"
 			return 1
 		fi
-		return 0
 	else
-		LAST_ERROR="Game Data Packer is not installed, cannot download game data"
+		LAST_ERROR="WGet is not installed, cannot download game"
 		return 1
 	fi
 }
@@ -216,8 +282,8 @@ function install_quake_3_hi-res_textures () {
 			echo "Downloadin high-resolution textures..."
 			wget -O xcsv_hires.zip https://files.ioquake3.org/xcsv_hires.zip
 			
-			echo "Extracting textures to ~/.q3ademo/demoq3"
-			unzip -o -d "$HOME/.q3ademo/demoq3" xcsv_hires.zip
+			echo "Extracting textures to ~/.q3a/baseq3"
+			unzip -o -d "$HOME/.q3a/baseq3" xcsv_hires.zip
 			
 			sudo rm -rf xcsv_hires.zip
 			
@@ -233,7 +299,6 @@ function install_quake_3_hi-res_textures () {
 }
 
 function install_quake_3_cfg () {
-	echo "Writing custom autoexec config to $HOME/.q3ademo/demoq3/autoexec.cfg..."
 	
 	cfg="bind p \"quit\"
 bind b \"addbot random 5\"
@@ -250,7 +315,7 @@ set sensitivity \"1.8\"
 
 set cg_shadows \"1\"
 set cg_autoswitch \"0\"
-set cg_fov \"110\"
+seta cg_fov \"115\"
 set cg_zoomfov \"38\"
 set cg_zoomScaling \"0\"
 set cg_zoomSensitivity \"1.35\"
@@ -318,14 +383,10 @@ seta r_texturebits \"32\"
 seta r_textureMode \"GL_LINEAR_MIPMAP_LINEAR\"
 seta r_vertexlight \"0\""
 	
-	# For the full game
+	# Write the config
+	echo "Writing Quake 3 config to $HOME/.q3a/baseq3/..."
 	sudo mkdir -p "$HOME/.q3a/baseq3/"
 	sudo echo -e "$cfg" > "$HOME/.q3a/baseq3/q3config.cfg"
-	
-	# Link cfg from full game to demo
-	sudo mkdir -p "$HOME/.q3ademo/demoq3/"
-	sudo rm -rf "$HOME/.q3ademo/demoq3/q3config.cfg"
-	sudo ln -s "$HOME/.q3a/baseq3/q3config.cfg" "$HOME/.q3ademo/demoq3/q3config.cfg"
 	
 	return 0
 }
