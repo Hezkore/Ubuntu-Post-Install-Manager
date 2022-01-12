2e2f4f8b1554a435f0f751e734e5717bfe1e088c
hezkore <hezkore@gmail.com>
2022-01-12 12:40:14 +0100

    added appimage launcher to software

--- src/software_menu.sh
+++ src/software_menu.sh
@@ -9,6 +9,7 @@ function show_software_menu () {
 		"Install_GNOME_Shell_Ext_Support" "Install GNOME shell extension support" "ON"
 		"Install_GNOME_Ext_Installer" "Install GNOME extension installer" "ON"
 		"Install_Discover_Store" "Install KDE Discover software store" "OFF"
+		"Install_AppImage_Launcher" "Install AppImage Launcher via custom PPA" "ON"
 		"Install_Remote_Desktop" "Install Remmina VNC and remote desktop" "ON"
 		"Install_Curl" "Install cURL CLI tool" "ON"
 		"Install_Edge_Browser" "Install Microsoft Edge Browser via custom PPA" "ON"
@@ -234,6 +235,11 @@ function install_innoextract () {
 
 function install_ppa_purge () {
 	sudo apt install ppa-purge -y
+}
+
+function install_appimage_launcher () {
+	sudo add-apt-repository ppa:appimagelauncher-team/stable -y
+	sudo apt install appimagelauncher -y
 }
 
 function install_remote_desktop () {
